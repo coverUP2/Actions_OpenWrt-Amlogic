035a0e73475d71ff386284c04f8f852464d07d7f
coverUP2 <73152816+coverUP2@users.noreply.github.com>
2023-08-29 18:35:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -24,11 +24,11 @@ sed -i '$a src-git coverup https://github.com/coverUP2/openwrt-packages.git' fee
 
 
 ## 解除系统限制
-ulimit -u 10000
-ulimit -n 4096
-ulimit -d unlimited
-ulimit -m unlimited
-ulimit -s unlimited
-ulimit -t unlimited
-ulimit -v unlimited
+#ulimit -u 10000
+#ulimit -n 4096
+#ulimit -d unlimited
+#ulimit -m unlimited
+#ulimit -s unlimited
+#ulimit -t unlimited
+#ulimit -v unlimited
 
